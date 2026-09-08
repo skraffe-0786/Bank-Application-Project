13e4dce78d5217c59ee8d05307a87670e2fc4956
shaik Ahmed Raffe <skraffe446@gmail.com>
2026-09-08 09:26:02 +0530

    updating DRF

--- BANK/serializers.py
+++ BANK/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import User_Account
 
+
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model=User_Account
         fields= '__all__'
+    def validate_Current_Balance(self,value):
+        if value < 0 :
+            raise serializers.ValidationError(
+                "Balance  cannot be  negative "
+            )
+        return value
